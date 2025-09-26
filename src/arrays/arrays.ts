8d4c0fb51304e3857e9aa28176f411714f548932
thomaslucking <telucking@gmail.com>
2025-09-26 08:40:44 +0200

    fix(comments) deleted intructions comment in arrays

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -6,16 +6,14 @@
  * As you will see, TypeScript will considerably
  * improve the autocomplete experience in your editor.
  */
-/*
- TODO: REMIND BASTIEN FOR INTRUCTIONS FRIDAY
- Write a function that takes an array of numbers and a threshold number as parameters, then returns a new array containing only the numbers that are greater than or equal to the threshold (removing any numbers below the threshold). For example, given [4, 1, 2] with threshold 2, it should return [4, 2] because 1 is below the threshold.
-*/
+
 export function removeNumbersBelowThreshold(
   array: number[],
   threshold: number,
 ): number[] | [] {
   // Write your code here
   return array.filter((num) => num >= threshold);
+  
 }
 
 export function filterStringByLength(
